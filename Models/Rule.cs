b095d927036b3edb67e691cbce46f732eb6bfd80
Eric McCracken <clanmccracken22@gmail.com>
2019-11-21 20:42:26 -0600

    Add project files.

---
+++ Models/Rule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusterPoint.Models
+{
+    public class Rule
+    {
+        public string RuleName { get; set; }
+        public string RuleText { get; set; }
+    }
+}

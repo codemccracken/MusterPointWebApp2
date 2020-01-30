79796b74cc06ba843c803696d42dc99e34cca5f0
Eric McCracken <clanmccracken22@gmail.com>
2020-01-30 16:38:06 -0600

    created the Item Class

---
+++ Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusterPoint.Models
+{
+    public class Item
+    {
+        public int ItemNumber { get; set; }
+        public string ItemName { get; set; }
+        public int ItemCost { get; set; }
+        public Array ItemProfiles { get; set; }
+    }
+}
